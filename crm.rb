292146dd61cf51b985404cc177d7020f17a7711e
Leon <leon.karho.chung@gmail.com>
2017-01-24 16:01:56 -0500

    add fake date, iterate in erb

--- crm.rb
+++ crm.rb
@@ -1,7 +1,15 @@
 # Implement the new web-based CRM here.
 # Do NOT copy the CRM class from the old crm assignment, as it won't work at all for the web-based version!
 # You'll have to implement it from scratch.
+require_relative 'contact'
 require 'sinatra'
+
+# fake date
+
+Contact.create('Leon', 'Chung', 'leon@bitmaker.com', 'pingpong champ')
+Contact.create('Honey', 'Baby', 'dogs@montreal.com', 'dogs of montreal')
+Contact.create('Web', 'Dev', 'webdev@code.com', 'jr')
+
 
 get '/' do
   @crm_app_name = "Leon's CRM"
