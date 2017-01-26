9647f9489b8597fada7e9e87a163e578ef01bb99
Leon <leon.karho.chung@gmail.com>
2017-01-26 17:16:47 -0500

    update and delete method functioning

--- crm.rb
+++ crm.rb
@@ -38,11 +38,21 @@ end
 get '/contacts/:id' do
   @contact = Contact.find(params[:id].to_i)
   if @contact
-    @contact.first_name = params[:first_name]
-    @contact.last_name = params[:last_name]
-    @contact.email = params[:email]
-    @contact.note = params[:note]
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
 
+put '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    @contact = Contact.update(
+    @contact.id,
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note])
     redirect to('/contacts')
   else
     raise Sinatra::NotFound
